e82a17743e6be838cc1161f805af263b72d29ed5
mebaysan <menesbaysan@gmail.com>
2019-08-17 02:38:36 +0300

    php ile pdo bağlantı açma

---
+++ 13-Php_PDO/1-1-PdoIleVeritabaniBaglantisiOlusturmak/baglanti.php
@@ -0,0 +1,2 @@
+<?php
+require_once 'baglantiacma.php'; // bağlantıyı sağladığımız dosyayı burada çağırdık.
---
+++ 13-Php_PDO/1-1-PdoIleVeritabaniBaglantisiOlusturmak/baglantiacma.php
@@ -0,0 +1,3 @@
+<?php
+
+$db = new PDO("mysql:host=localhost;dbname=dersler", "root", ""); // bir instance üretiyoruz. ilk parametre database adresi, ikinci parametre kullanıcı adı(varsa), üçüncü parametre şifre (varsa)
