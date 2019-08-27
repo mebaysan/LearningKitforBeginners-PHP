089ec4f30619277c9d592b68f2ab6ecc05e87658
mebaysan <menesbaysan@gmail.com>
2019-08-27 14:24:07 +0300

    delete project

--- loginProject/functions/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-ob_start();
-session_start();
-include "functions.php";
-include "db.php";
-if (!$con) { // eğer con nesnesi false gelirse veya yoksa ekrana hata vereceğiz
-    echo "<div class='alert alert-danger'>Hata! Veritabanı bağlantısı Sağlanamadı</div>";
-}
