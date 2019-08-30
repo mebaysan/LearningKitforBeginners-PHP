4f6a78b7123beab61cfd93c16db87ca8d0b01bd8
mebaysan <menesbaysan@gmail.com>
2019-08-30 12:41:59 +0300

    php ftp

--- 21-Php_FTP/2-FTPdosyaolusturmasilme.php
+++ 21-Php_FTP/2-FTPdosyaolusturmasilme.php
@@ -1,14 +1,20 @@
 <?php
-// ftp_mkdir
-// ftp_rmdir
-// ftp_chdir
-// ftp_pwd
-$conn_id = ftp_connect("192.168.1.1"); // bize bir connection id'si döndürür
+// ftp_mkdir -> dizin oluşturur
+// ftp_rmdir -> dizin siler
+// ftp_chdir -> mevcut dizini değiştirir
+// ftp_pwd   -> mevcut dizinin yolunu döndürür
+$conn_id = ftp_connect("192.168.1.61"); // bize bir connection id'si döndürür
 if ($conn_id) {
     echo 'FTP bağlantısı başarılı' . "<br>";
     $login = ftp_login($conn_id, "user_name", "user_password");
     if ($login) {
-        echo "Başarılı bir şekilde $conn_id'e giriş yapıldı";
+        echo "Başarılı bir şekilde $conn_id'e giriş yapıldı" . "<br>";
+        ftp_chdir($conn_id, "./public.html"); // connection'daki bu dosyaya gider
+        $create = ftp_mkdir($conn_id, "udemy"); // başarılı olursa bize oluşturulan dosyanın adını döner
+        $create ? print("$create oluşturuldu") : print("$create oluşturulamadı");
+        $delete = ftp_rmdir($conn_id, "udemy"); // connection'ı alır ve silenecek dosya adını alır
+        $delete ? print("$create silindi") : print("$create silinemedi");
+        echo ftp_pwd($conn_id);
     } else {
         $error = error_get_last();
         echo "$conn_id'e giriş yapılamadı! Hata : " . $error['message'];
