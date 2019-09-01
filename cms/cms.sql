924fd6811311793e250e27b6acfa052e8b2f645a
mebaysan <menesbaysan@gmail.com>
2019-09-01 23:35:13 +0300

    add posts table mysql

--- cms/cms.sql
+++ cms/cms.sql
@@ -38,6 +38,37 @@ LOCK TABLES `categories` WRITE;
 INSERT INTO `categories` VALUES (1,'PHP'),(2,'Python'),(3,'Java');
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posts` (
+  `post_id` int(11) NOT NULL AUTO_INCREMENT,
+  `post_category_id` int(11) DEFAULT NULL,
+  `post_title` varchar(255) DEFAULT NULL,
+  `post_author` varchar(245) DEFAULT NULL,
+  `post_date` date DEFAULT NULL,
+  `post_image` varchar(345) DEFAULT NULL,
+  `post_content` longtext,
+  `post_tags` varchar(245) DEFAULT NULL,
+  `post_comment_count` int(11) DEFAULT NULL,
+  `post_status` int(11) DEFAULT '0',
+  PRIMARY KEY (`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -48,4 +79,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-09-01  1:04:18
+-- Dump completed on 2019-09-01 23:34:38
