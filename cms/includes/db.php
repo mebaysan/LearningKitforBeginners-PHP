ba8b750d019e071d75adbb0b2d662682c1a615de
mebaysan <menesbaysan@gmail.com>
2019-09-03 00:25:06 +0300

    deleted cms

--- cms/includes/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "2976";
-$db_name = "cms";
-$connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-if (!$connection)
-    echo "<div class='alert alert-danger'>Veritabanı Bağlantısı Sağlanamadı!</div>";
--- cms/includes/widget.php
+++
@@ -1,4 +0,0 @@
-<div class="well">
-    <h4>Side Widget Well</h4>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, ratione.</p>
-</div>
